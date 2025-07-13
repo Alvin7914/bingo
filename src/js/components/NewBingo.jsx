8413ee36d543526cc4ecf6d1523b8c6429ebd6a5
Alvin7914 <krzysztof.kamieniarz791@wp.pl>
2025-07-14 00:02:57 +0200

    prace w toku

---
+++ src/js/components/NewBingo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NewBingo = () => {
+
+
+    return (
+<main className="new-bingo">Nowe Bingo</main>
+    )
+}
+
+export default NewBingo;
