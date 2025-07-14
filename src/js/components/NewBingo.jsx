33931dfeac9b1051e7c36b2466bd191c3ffb57ba
Alvin7914 <krzysztof.kamieniarz791@wp.pl>
2025-07-14 16:32:44 +0200

    dodawanie wpisow do stanu i localStorage

--- src/js/components/NewBingo.jsx
+++ src/js/components/NewBingo.jsx
@@ -7,11 +7,22 @@ const NewBingo = () => {
     useEffect(() => {
         if (localStorage.getItem('entriesList') !== null) {
             setEntriesList(JSON.parse(localStorage.getItem('entriesList')))
-        } else {
-            setEntriesList(['Pierwsze zdanie', 'Drugi wpis', 'Trzecie hasło', 'Czwarta opcja'])
-        }
+        } 
+        
+        // else {
+        //     setEntriesList(['Pierwsze zdanie', 'Drugi wpis', 'Trzecie hasło', 'Czwarta opcja']) // tymczasowo zeby cos sie wyswietlalo na stronie
+        // }
 
     }, []);
+
+    const addEntry = () => {
+        const newEntry = document.querySelector('#new-entry-input');
+        setEntriesList(state => [...state, newEntry.value]);
+        localStorage.setItem('entriesList', JSON.stringify([...entriesList, newEntry.value]))
+        console.log(newEntry);
+        
+        // newEntry.value = '';
+    }
 
 
 
@@ -23,12 +34,12 @@ const NewBingo = () => {
                 <h2 className="new-bingo__form-h2">Hasła: {entriesList.length}</h2>
                 <div className="new-bingo__form-box">
                     <ul className="new-bingo__form-list">
-                        {entriesList.map((item, i) => <li className="new-bingo__form-list__lis" key={i}><span>{item}</span><svg className="new-bingo__form__trash-svg" width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#056b05"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9.17065 4C9.58249 2.83481 10.6937 2 11.9999 2C13.3062 2 14.4174 2.83481 14.8292 4" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M20.5 6H3.49988" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M18.3735 15.3991C18.1965 18.054 18.108 19.3815 17.243 20.1907C16.378 21 15.0476 21 12.3868 21H11.6134C8.9526 21 7.6222 21 6.75719 20.1907C5.89218 19.3815 5.80368 18.054 5.62669 15.3991L5.16675 8.5M18.8334 8.5L18.6334 11.5" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M9.5 11L10 16" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M14.5 11L14 16" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> </g></svg></li>)}
+                        {entriesList.map((item, i) => <li className="new-bingo__form-list__lis" key={i}><span>{item}</span><svg className="new-bingo__form__trash-svg" width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#056b05"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9.17065 4C9.58249 2.83481 10.6937 2 11.9999 2C13.3062 2 14.4174 2.83481 14.8292 4" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round"></path> <path d="M20.5 6H3.49988" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round"></path> <path d="M18.3735 15.3991C18.1965 18.054 18.108 19.3815 17.243 20.1907C16.378 21 15.0476 21 12.3868 21H11.6134C8.9526 21 7.6222 21 6.75719 20.1907C5.89218 19.3815 5.80368 18.054 5.62669 15.3991L5.16675 8.5M18.8334 8.5L18.6334 11.5" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round"></path> <path d="M9.5 11L10 16" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round"></path> <path d="M14.5 11L14 16" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round"></path> </g></svg></li>)}
                         <li className="form__add">
-                            <input type="text" placeholder="Wpisz nowe hasło" className="new-bingo__form__input-entry" />
+                            <input type="text" placeholder="Wpisz nowe hasło" className="new-bingo__form__input-entry" id="new-entry-input" />
                             <div className="plus-sign">
-                                <span className='plus-1'></span>
-                                <span className='plus-2'></span>
+                                <span className='plus-1' onClick={addEntry}></span>
+                                <span className='plus-2' onClick={addEntry}></span>
                             </div>
                         </li>
                     </ul>
