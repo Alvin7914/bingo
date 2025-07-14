8c50e869118308368916f3ab4ca3e9ee0c70da5a
Alvin7914 <krzysztof.kamieniarz791@wp.pl>
2025-07-14 02:32:06 +0200

    nowe bingo - stylkowanie

--- src/js/components/NewBingo.jsx
+++ src/js/components/NewBingo.jsx
@@ -7,8 +7,11 @@ const NewBingo = () => {
     const [entriesList, setEntriesList] = useState([]);
 
     useEffect(() => {
-
-        setEntriesList(['a', 'b', 'c']);
+        if (localStorage.getItem('entriesList') !== null) {
+            setEntriesList(JSON.parse(localStorage.getItem('entriesList')))
+        } else {
+            setEntriesList(['a', 'b', 'c', 'd'])
+        }
 
     }, []);
 
@@ -18,16 +21,19 @@ const NewBingo = () => {
 <main className="new-bingo">
 
     <form className="new-bingo__form">
-
-        <input type="text" placeholder="Nazwa" />
-        <h2>Hasła:</h2>
-        <div>
+        <input type="text" placeholder="Nazwa planszy" className="new-bingo__form__input-name"/>
+        <h2 className="new-bingo__form-h2">Hasła:</h2>
+        <ul className="new-bingo__form-list">
             {entriesList.map( item => <li>{item}</li>)}
-        </div>
-
-
+            <li className="form__add">
+                <input type="text" placeholder="Wpisz nowe hasło" className="new-bingo__form__input-entry"/>
+                <div className="plus-sign">
+                    <span className='plus-1'></span>
+                    <span className='plus-2'></span>
+                </div>
+            </li>
+        </ul>
     </form>
-
 </main>
     )
 }
