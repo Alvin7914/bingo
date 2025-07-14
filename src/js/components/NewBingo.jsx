3d1b548049700356337bd702fddaa3eb7a0ffe91
Alvin7914 <krzysztof.kamieniarz791@wp.pl>
2025-07-14 15:55:59 +0200

    gotowe stylowanie dla nowego bingo

--- src/js/components/NewBingo.jsx
+++ src/js/components/NewBingo.jsx
@@ -8,7 +8,7 @@ const NewBingo = () => {
         if (localStorage.getItem('entriesList') !== null) {
             setEntriesList(JSON.parse(localStorage.getItem('entriesList')))
         } else {
-            setEntriesList(['a', 'b', 'c', 'd'])
+            setEntriesList(['Pierwsze zdanie', 'Drugi wpis', 'Trzecie hasło', 'Czwarta opcja'])
         }
 
     }, []);
@@ -20,18 +20,23 @@ const NewBingo = () => {
 
             <form className="new-bingo__form">
                 <input type="text" placeholder="Nazwa planszy" className="new-bingo__form__input-name" />
-                <h2 className="new-bingo__form-h2">Hasła:</h2>
-                <ul className="new-bingo__form-list">
-                    {entriesList.map((item, i) => <li key={i}>{item}</li>)}
-                    <li className="form__add">
-                        <input type="text" placeholder="Wpisz nowe hasło" className="new-bingo__form__input-entry" />
-                        <div className="plus-sign">
-                            <span className='plus-1'></span>
-                            <span className='plus-2'></span>
-                        </div>
-                    </li>
-                </ul>
-                <button className="new-bingo__form-saveBtn">Zapisz</button>
+                <h2 className="new-bingo__form-h2">Hasła: {entriesList.length}</h2>
+                <div className="new-bingo__form-box">
+                    <ul className="new-bingo__form-list">
+                        {entriesList.map((item, i) => <li className="new-bingo__form-list__lis" key={i}><span>{item}</span><svg className="new-bingo__form__trash-svg" width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#056b05"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9.17065 4C9.58249 2.83481 10.6937 2 11.9999 2C13.3062 2 14.4174 2.83481 14.8292 4" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M20.5 6H3.49988" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M18.3735 15.3991C18.1965 18.054 18.108 19.3815 17.243 20.1907C16.378 21 15.0476 21 12.3868 21H11.6134C8.9526 21 7.6222 21 6.75719 20.1907C5.89218 19.3815 5.80368 18.054 5.62669 15.3991L5.16675 8.5M18.8334 8.5L18.6334 11.5" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M9.5 11L10 16" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M14.5 11L14 16" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> </g></svg></li>)}
+                        <li className="form__add">
+                            <input type="text" placeholder="Wpisz nowe hasło" className="new-bingo__form__input-entry" />
+                            <div className="plus-sign">
+                                <span className='plus-1'></span>
+                                <span className='plus-2'></span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="new-bingo__form__btn-box">
+                    <button className="new-bingo__form-clearBtn">Wyczyść wszystko</button>
+                    <button className="new-bingo__form-saveBtn">Zapisz</button>
+                </div>
             </form>
         </main>
     )
