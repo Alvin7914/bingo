8391d2c7ecbfe9a4467d0cd42a8d94a25367e68a
Alvin7914 <krzysztof.kamieniarz791@wp.pl>
2025-07-15 02:22:42 +0200

    poprawki vite/package.json

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 
-const exercisePath = "C:\\Users\\user3\\OneDrive\\Pulpit\\bingo";
+// const exercisePath = "C:\\Users\\user3\\OneDrive\\Pulpit\\bingo";
 
 
 export default defineConfig({
-    root: exercisePath,
-    server: {
-        port: 3000,
-    },
+    // root: exercisePath,
+    // server: {
+    //     port: 3000,
+    // },
     plugins: [react()],
     base: 'bingo',
 });
