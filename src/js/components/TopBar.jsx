77375c20b2a3a04b133eb3804244ca092f6abcfd
Alvin7914 <krzysztof.kamieniarz791@wp.pl>
2025-07-15 02:47:20 +0200

    poprawki

--- src/js/components/TopBar.jsx
+++ src/js/components/TopBar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import leaf from "./assets/leaf3.png"
 
 const TopBar = () => {
 
 
     return (
 <header className="header">
-    <Link to='/'><img src='src/assets/leaf3.png' alt='Leaf' className="leaf-image"/></Link>
+    <Link to='/'><img src={leaf} alt='Leaf' className="leaf-image"/></Link>
     <nav className="header__nav">
         <ul className="header__nav__list">
             <li className="header__nav__list-element"><Link to="/new">Nowe bingo</Link></li>
