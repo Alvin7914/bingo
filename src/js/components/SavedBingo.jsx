7b706ade4a23d111c86f022d9f5d2dfde592a98c
Alvin7914 <krzysztof.kamieniarz791@wp.pl>
2025-07-17 00:26:51 +0200

    mixin, część HTML i stylowania do zapisanych gier

--- src/js/components/SavedBingo.jsx
+++ src/js/components/SavedBingo.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const SavedBingo = () => {
+    const [savedGames, setSavedGames] = useState([]);
 
+    useEffect(() => {
+        if (localStorage.getItem('savedGames') !== null) {
+            setSavedGames(JSON.parse(localStorage.getItem('savedGames')))
+        }
+
+    }, []);
 
     return (
-        <div className="saved-bingo">
-            Zapisane Bingo
-        </div>
+        <aside className="saved-bingo">
+            <section className="saved-bingo__box">
+                <ul className="saved-bingo__box__list">
+                    {savedGames.map((game, index) => <li id={index} className="saved-bingo__box__list-lis" key={game.name}>
+                        
+                            <span className="saved-bingo__box__list-lis__name">{game.name}</span>
+                            <ul className="saved-bingo__box__list-lis__entries">
+                                {game.entries.map((entry, index) => <li id={index} key={entry.entry}>{entry.entry}</li>)}
+                            </ul>
+                        
+                    </li>
+                    )}
+                </ul>
+            </section>
+        </aside>
     )
 }
 
